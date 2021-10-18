137e7a4c9a7180e491a491ee50d7f1bbfa99d6e1
anhkhoi1511619 <anhkhoi1511619@gmail.com>
2021-10-19 00:23:51 +0900

    MVC Model最初の作成

--- app/src/main/java/com/example/listview/DataAdapterInfo.java
+++ app/src/main/java/com/example/listview/DataAdapterInfo.java
@@ -1,2 +1,39 @@
-package com.example.listview;public class DataAdapterInfo {
+package com.example.listview;
+
+import java.util.ArrayList;
+
+public class DataAdapterInfo {
+
+    private ArrayList<ListItem> listItems;
+
+    public static class ListItem{
+        public long no;
+        public int Status;
+        public String name;
+        public String detail;
+//        public DataAdapterInfo dataAdapterInfo = new DataAdapterInfo();
+    }
+
+    private static DataAdapterInfo instance = new DataAdapterInfo();
+    public static DataAdapterInfo getInstance(){
+        return instance;
+    }
+
+    private DataAdapterInfo(){
+        listItems = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<ListItem> getListItems() {
+        return listItems;
+    }
+
+
+    /**
+     *
+     */
+
 }
